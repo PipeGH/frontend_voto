cce098e6b15911c370eaf64eba951f3123b30b54
Daniel Guzmán <danieljk001@yahoo.com>
2023-10-27 21:08:15 -0500

    rutas y disposición

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistAvanceComponent } from './regist-avance/regist-avance.component';
+import { TablasEspecificasComponent } from './tablas-especificas/tablas-especificas.component';
+import { TablasGeneralesComponent } from './tablas-generales/tablas-generales.component';
 
 //, canActivate: [authGuard]
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'registro',
+    component: RegistAvanceComponent,
+  },
+  {
+    path: 'generales',
+    component: TablasGeneralesComponent,
+  },
+  {
+    path: 'especificos',
+    component: TablasEspecificasComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from './services/auth.service';
 import Swal from 'sweetalert2';
 import decode from 'jwt-decode';
@@ -7,10 +12,9 @@ import decode from 'jwt-decode';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', './menu.component.css']
+  styleUrls: ['./app.component.css', './menu.component.css'],
 })
 export class AppComponent implements OnInit {
-
   token: any = localStorage.getItem('token');
   usuario: string = '';
   menuOpen: boolean = false;
@@ -22,7 +26,7 @@ export class AppComponent implements OnInit {
     return this.formUser.get('password') as FormControl;
   }
 
-  constructor(private fb: FormBuilder, private service: AuthService) { }
+  constructor(private fb: FormBuilder, private service: AuthService) {}
 
   ngOnInit(): void {
     if (this.token) {
@@ -32,91 +36,94 @@ export class AppComponent implements OnInit {
 
   formUser = this.fb.group({
     usuario: ['', Validators.required],
-    password: ['', Validators.required]
-
+    password: ['', Validators.required],
   });
-
 
   mostrar(): void {
     const data = this.formUser.value as FormGroup;
-    this.service.login(data).subscribe((res: any) => {
-
-      switch (res.message) {
-        case 'logged in session successful':
-          Swal.fire({
-            icon: 'success',
-            text: 'Bienvenido al sistema',
-          }).then(() => {
-            const token = res.token;
-            localStorage.setItem("token", token);
-            this.cargar();
-            window.location.reload();
-          });
-          break;
-        case 'No autorizado, password erronea':
-          Swal.fire({
-            icon: 'error',
-            text: 'Contraseña erronea, intente nuevamente',
-          });
-          break;
-        case 'No autorizado, email no registrado.':
-          Swal.fire({
-            icon: 'error',
-            text: 'usuario erroneo, intente nuevamente',
-          });
-          break;
+    this.service.login(data).subscribe(
+      (res: any) => {
+        switch (res.message) {
+          case 'logged in session successful':
+            Swal.fire({
+              icon: 'success',
+              text: 'Bienvenido al sistema',
+            }).then(() => {
+              const token = res.token;
+              localStorage.setItem('token', token);
+              this.cargar();
+              window.location.reload();
+            });
+            break;
+          case 'No autorizado, password erronea':
+            Swal.fire({
+              icon: 'error',
+              text: 'Contraseña erronea, intente nuevamente',
+            });
+            break;
+          case 'No autorizado, email no registrado.':
+            Swal.fire({
+              icon: 'error',
+              text: 'usuario erroneo, intente nuevamente',
+            });
+            break;
+        }
+      },
+      (error: any) => {
+        Swal.fire({
+          icon: 'error',
+          text: 'Actualmente no se puede realizar la peticion, intente mas tarde',
+        });
       }
-    }, (error: any) => {
-      Swal.fire({
-        icon: 'error',
-        text: 'Actualmente no se puede realizar la peticion, intente mas tarde',
-      })
-    });
+    );
   }
   cargar() {
     let decodetoken: any = {};
-    let token: any = localStorage.getItem("token");
+    let token: any = localStorage.getItem('token');
     decodetoken = decode(token);
     const { usuario } = decodetoken.data;
     this.usuario = usuario;
   }
   closeMenu() {
-    this.menuOpen = true;
+    if (window.innerWidth <= 720) {
+      this.menuOpen = true;
+    }
   }
   close_session() {
     const swalWithBootstrapButtons = Swal.mixin({
       customClass: {
         confirmButton: 'btn btn-success',
-        cancelButton: 'btn btn-danger'
+        cancelButton: 'btn btn-danger',
       },
-      buttonsStyling: false
-    })
+      buttonsStyling: false,
+    });
 
-    swalWithBootstrapButtons.fire({
-      text: "Esta seguro de cerrar sesion",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Si',
-      cancelButtonText: 'No',
-      reverseButtons: true
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire({
-          icon: 'success',
-          text: 'Nos veremos pronto',
-        }).then(() => {
-          localStorage.removeItem("token");
-          window.location.reload();
-        })
-      } else if (
-        /* Read more about handling dismissals below */
-        result.dismiss === Swal.DismissReason.cancel
-      ) {
-        Swal.fire({
-          text: 'Continuamos',
-        })
-      }
-    })
+    swalWithBootstrapButtons
+      .fire({
+        text: 'Esta seguro de cerrar sesion',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No',
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            icon: 'success',
+            text: 'Nos veremos pronto',
+          }).then(() => {
+            localStorage.removeItem('token');
+            window.location.reload();
+          });
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          Swal.fire({
+            text: 'Continuamos',
+          });
+        }
+      });
   }
-
 }
