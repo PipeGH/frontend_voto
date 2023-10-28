9c2e97802f64be7fa1c5255029f73be886908395
jhoanfelipe1919@gmail.com <jhoanfelipe1919@gmail.com>
2023-10-28 15:07:43 -0500

    Implementación de las graficas

--- src/app/tablas-especificas/tablas-especificas.component.ts
+++ src/app/tablas-especificas/tablas-especificas.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SitiosService } from '../services/sitios.service';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-tablas-especificas',
@@ -8,11 +9,101 @@ import { SitiosService } from '../services/sitios.service';
 })
 export class TablasEspecificasComponent implements OnInit {
   sitios: any[] = [];
+  public votantes: any;
+  public myChart: any;
+  createChart() {
+    this.votantes = document.getElementById('myChart');
+    this.myChart = new Chart("votantes", {
+      type: 'line',
+      data: {
+        labels: ['Agua Bonita', 'Silvania-coliseo', 'Silvania-colegio', 'Subia'],
+        datasets: [{
+          label: 'Total de Votantes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1,
+          pointRadius: 8,
+        }]
+      },
+
+
+      options: {
+        scales: {
+          y: {
+
+            beginAtZero: true
+          },
+          x: {
+            ticks: {
+              stepSize: 1,
+              font: {
+                size: 16
+              }
+            }
+          }
+        }
+      }
+    });
+  }
+  public votos: any;
+  public myChart1: any;
+  createChart1() {
+    this.votos = document.getElementById('myChart1');
+    this.myChart1 = new Chart("votos", {
+      type: 'line',
+      data: {
+        labels: ['Agua Bonita', 'Silvania-coliseo', 'Silvania-colegio', 'Subia'],
+        datasets: [{
+          label: 'Total de Votos',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(54, 162, 235, 1)',
+          ],
+          borderColor: [
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1,
+          pointRadius: 8
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+
+            beginAtZero: true
+          },
+          x: {
+            ticks: {
+              stepSize: 1,
+              font: {
+                size: 16
+              }
+            }
+          }
+        }
+      }
+    });
+  }
 
   constructor(private service: SitiosService) { }
-
   ngOnInit(): void {
     this.chargeData();
+    this.createChart();
+    this.createChart1();
   }
   chargeData() {
     this.service.getLugares().subscribe((res: any) => {
@@ -21,5 +112,4 @@ export class TablasEspecificasComponent implements OnInit {
       }
     });
   }
-
 }
--- src/app/tablas-generales/tablas-generales.component.ts
+++ src/app/tablas-generales/tablas-generales.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-tablas-generales',
   templateUrl: './tablas-generales.component.html',
   styleUrls: ['./tablas-generales.component.css']
 })
-export class TablasGeneralesComponent {
+export class TablasGeneralesComponent implements OnInit {
 
+
+  ngOnInit(): void {
+
+  }
 }
