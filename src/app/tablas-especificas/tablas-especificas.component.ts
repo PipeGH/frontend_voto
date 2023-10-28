9291a52952cd156a0d30624089d1a02acd714e50
Daniel Guzmán <danieljk001@yahoo.com>
2023-10-28 16:40:49 -0500

    Creación de graficas

--- src/app/tablas-especificas/tablas-especificas.component.ts
+++ src/app/tablas-especificas/tablas-especificas.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { GeneralesService } from '../services/generales.service';
-import { NumberSymbol } from '@angular/common';
-=======
-import { SitiosService } from '../services/sitios.service';
-import Chart from 'chart.js/auto';
->>>>>>> 8e5cd240d7dbb6b4db26d903531eabb7eb211ec6
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-tablas-especificas',
@@ -14,7 +9,6 @@ import Chart from 'chart.js/auto';
 })
 export class TablasEspecificasComponent implements OnInit {
   sitios: any[] = [];
-<<<<<<< HEAD
   mesas: any[] = [];
   lista: any = [];
   ciudad_id: number = 0;
@@ -33,99 +27,6 @@ export class TablasEspecificasComponent implements OnInit {
 
   constructor(private service: GeneralesService) {}
 
-=======
-  public votantes: any;
-  public myChart: any;
-  createChart() {
-    this.votantes = document.getElementById('myChart');
-    this.myChart = new Chart("votantes", {
-      type: 'line',
-      data: {
-        labels: ['Agua Bonita', 'Silvania-coliseo', 'Silvania-colegio', 'Subia'],
-        datasets: [{
-          label: 'Total de Votantes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 1)',
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1,
-          pointRadius: 8,
-        }]
-      },
-
-
-      options: {
-        scales: {
-          y: {
-
-            beginAtZero: true
-          },
-          x: {
-            ticks: {
-              stepSize: 1,
-              font: {
-                size: 16
-              }
-            }
-          }
-        }
-      }
-    });
-  }
-  public votos: any;
-  public myChart1: any;
-  createChart1() {
-    this.votos = document.getElementById('myChart1');
-    this.myChart1 = new Chart("votos", {
-      type: 'line',
-      data: {
-        labels: ['Agua Bonita', 'Silvania-coliseo', 'Silvania-colegio', 'Subia'],
-        datasets: [{
-          label: 'Total de Votos',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(54, 162, 235, 1)',
-          ],
-          borderColor: [
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1,
-          pointRadius: 8
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-
-            beginAtZero: true
-          },
-          x: {
-            ticks: {
-              stepSize: 1,
-              font: {
-                size: 16
-              }
-            }
-          }
-        }
-      }
-    });
-  }
-
-  constructor(private service: SitiosService) { }
->>>>>>> 8e5cd240d7dbb6b4db26d903531eabb7eb211ec6
   ngOnInit(): void {
     this.chargeData();
     this.createChart();
@@ -194,7 +95,6 @@ export class TablasEspecificasComponent implements OnInit {
       }
     });
   }
-<<<<<<< HEAD
 
   onSelect(event: any): void {
     this.ciudad_id = event.target.value;
@@ -230,6 +130,102 @@ export class TablasEspecificasComponent implements OnInit {
   handlePageChange(event: any) {
     this.p = event;
   }
-=======
->>>>>>> 8e5cd240d7dbb6b4db26d903531eabb7eb211ec6
+
+  public votantes: any;
+  public myChart: any;
+  createChart() {
+    this.votantes = document.getElementById('myChart');
+    this.myChart = new Chart('votantes', {
+      type: 'line',
+      data: {
+        labels: [
+          'Agua Bonita',
+          'Silvania-coliseo',
+          'Silvania-colegio',
+          'Subia',
+        ],
+        datasets: [
+          {
+            label: 'Total de Votantes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: ['rgba(255, 99, 132, 1)'],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+            pointRadius: 8,
+          },
+        ],
+      },
+
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+          x: {
+            ticks: {
+              stepSize: 1,
+              font: {
+                size: 16,
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+
+  public votos: any;
+  public myChart1: any;
+  createChart1() {
+    this.votos = document.getElementById('myChart1');
+    this.myChart1 = new Chart('votos', {
+      type: 'line',
+      data: {
+        labels: [
+          'Agua Bonita',
+          'Silvania-coliseo',
+          'Silvania-colegio',
+          'Subia',
+        ],
+        datasets: [
+          {
+            label: 'Total de Votos',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: ['rgba(54, 162, 235, 1)'],
+            borderColor: [
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+            pointRadius: 8,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+          x: {
+            ticks: {
+              stepSize: 1,
+              font: {
+                size: 16,
+              },
+            },
+          },
+        },
+      },
+    });
+  }
 }
